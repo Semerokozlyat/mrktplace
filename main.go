759b7c499d321775916d5a5e373818a6eafc7b10
evgeniy.pomortsev <evgeniy.pomortsev@acronis.com>
2022-10-29 23:49:48 +0300

    Added templates and execute func for home and contact pages

--- main.go
+++ main.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(rw http.ResponseWriter, r *http.Request) {
+func executeTemplate(rw http.ResponseWriter, templatePath string) {
 	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(rw, "<h1>Start page</h1>")
+	tpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		log.Printf("parsing template file: %v", err)
+		http.Error(rw, "Failed to parse template file", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(rw, nil)
+	if err != nil {
+		log.Printf("executing template file: %v", err)
+		http.Error(rw, "Failed to execute template file", http.StatusInternalServerError)
+		return
+	}
+}
+
+func homeHandler(rw http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(rw, tplPath)
 }
 
 func contactHandler(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(rw, "<h1>Contact page.</h1>"+
-		"<p>Email at <a href=\"mailto:example@example.com\">example@example.com</p>")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(rw, tplPath)
 }
 
 func main() {
